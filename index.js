cbf64dec40c7f744f3b4f7425b0e77202c3da3cd
Keith Mutugi <keithmutugi64@gmail.com>
2026-04-10 12:29:30 +0300

    updated index.js

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-
-
-
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
+const datejs = require('datejs');
+
+function combineUsers(...args) {
+  // initialize return object
+  let combinedObject = {
+    users: []
+  };
+
+  // loop through args and merge arrays
+  for (let arr of args) {
+    combinedObject.users.push(...arr);
+  }
+
+  // add current date in M/d/yyyy format
+  combinedObject.merge_date = new Date().toString("M/d/yyyy");
+
+  return combinedObject;
+}
+
+module.exports = combineUsers;
